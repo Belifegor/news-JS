ff2a76f7ccc0dc2ee45c056e93785aa38f7c16f0
github-usernamebelifegor <hydrox27@gmail.com>
2025-02-23 23:48:20 +0200

    feat: add interfaces and type safety to controller

--- news-api/src/components/controller/controller.ts
+++ news-api/src/components/controller/controller.ts
@@ -1,7 +1,8 @@
+import { NewsAPIResponse } from '../../types/interfaces';
 import AppLoader from './appLoader';
 
 class AppController extends AppLoader {
-    getSources(callback) {
+    getSources(callback: (data: NewsAPIResponse) => void): void {
         super.getResp(
             {
                 endpoint: 'sources',
@@ -10,13 +11,14 @@ class AppController extends AppLoader {
         );
     }
 
-    getNews(e, callback) {
-        let target = e.target;
-        const newsContainer = e.currentTarget;
+    getNews(e: Event, callback: (data: NewsAPIResponse) => void): void {
+        let target = e.target as HTMLElement; 
+        const newsContainer = e.currentTarget as HTMLElement;
 
         while (target !== newsContainer) {
             if (target.classList.contains('source__item')) {
                 const sourceId = target.getAttribute('data-source-id');
+
                 if (newsContainer.getAttribute('data-source') !== sourceId) {
                     newsContainer.setAttribute('data-source', sourceId);
                     super.getResp(
@@ -31,7 +33,7 @@ class AppController extends AppLoader {
                 }
                 return;
             }
-            target = target.parentNode;
+            target = target.parentNode as HTMLElement;
         }
     }
 }
---
+++ news-api/src/types/interfaces.ts
@@ -0,0 +1,32 @@
+export interface NewsArticle {
+    title: string;
+    description: string;
+    url: string;
+  }
+  
+  export interface NewsAPIResponse {
+    status: string;
+    totalResults: number;
+    articles: NewsArticle[];
+  }
+  
+  export interface Article {
+    source: {
+      id: string | null;
+      name: string;
+    };
+    author: string | null;
+    title: string;
+    description: string | null;
+    url: string;
+    urlToImage: string | null;
+    publishedAt: string;
+    content: string | null;
+  }
+  
+  export interface NewsResponse {
+    status: string;
+    totalResults: number;
+    articles: Article[];
+  }
+  
