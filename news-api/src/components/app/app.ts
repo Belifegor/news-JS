d3a520b9f07e37b777797ff756c1c0b321ddce1b
github-usernamebelifegor <hydrox27@gmail.com>
2025-02-24 01:38:44 +0200

    feat: add touch support with MouseEvent | TouchEvent + correct types in app.ts

--- news-api/src/components/app/app.ts
+++ news-api/src/components/app/app.ts
@@ -1,17 +1,27 @@
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
+import { NewsAPIResponse } from '../../types/interfaces';
 
 class App {
+    private controller: AppController;
+    private view: AppView;
+
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
     }
 
-    start() {
-        document
-            .querySelector('.sources')
-            .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
-        this.controller.getSources((data) => this.view.drawSources(data));
+    start(): void {
+        const sourcesElement = document.querySelector('.sources') as HTMLElement;
+
+        if (sourcesElement) {
+            sourcesElement.addEventListener('click', (e: Event) => {
+                const event = e as MouseEvent | TouchEvent;
+                this.controller.getNews(event, (data: NewsAPIResponse) => this.view.drawNews(data));
+            });
+        }
+
+        this.controller.getSources((data: NewsAPIResponse) => this.view.drawSources(data));
     }
 }
 
